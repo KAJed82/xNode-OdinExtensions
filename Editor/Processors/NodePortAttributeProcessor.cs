bd04f4ec51db657e6c096d0baeddebb6952574a1
Kailey Joanette <kaileyjo@bigbluebubble.com>
2020-04-29 18:11:03 -0400

    Force node port reference to be hidden.

---
+++ Editor/Processors/NodePortAttributeProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+
+using XNode;
+
+namespace XNodeEditor.Odin
+{
+    public class NodePortAttributeProcessor : OdinAttributeProcessor<NodePort>
+    {
+        public override bool CanProcessSelfAttributes( InspectorProperty property )
+        {
+            return true;
+        }
+
+        public override bool CanProcessChildMemberAttributes( InspectorProperty parentProperty, MemberInfo member )
+        {
+            return false;
+        }
+
+        public override void ProcessSelfAttributes( InspectorProperty property, List<Attribute> attributes )
+        {
+            attributes.Add( new HideReferenceObjectPickerAttribute() );
+        }
+    }
+}
