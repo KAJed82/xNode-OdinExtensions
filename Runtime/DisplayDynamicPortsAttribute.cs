7d7b82e69ba0232818492d88e0e14ffc9012cbde
Kailey Joanette <kaileyjo@bigbluebubble.com>
2020-05-01 22:43:12 -0400

    Missed a file.

--- Runtime/DisplayDynamicPortsAttribute.cs
+++ Runtime/DisplayDynamicPortsAttribute.cs
@@ -4,4 +4,20 @@
 /// Automatically draws extra 'loose' dynamic ports to the end of the node.
 /// </summary>
 [AttributeUsage( AttributeTargets.Class, AllowMultiple = false )]
-public class DisplayDynamicPortsAttribute : Attribute { }
+public class DisplayDynamicPortsAttribute : Attribute
+{
+	public bool ShowRemoveButton { get; private set; }
+
+	public DisplayDynamicPortsAttribute()
+	{
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="showRemoveButton">Set this to true to had a button automatically added to 'loose' dynamic ports to remove them.</param>
+	public DisplayDynamicPortsAttribute(bool showRemoveButton)
+	{
+		ShowRemoveButton = showRemoveButton;
+	}
+}
