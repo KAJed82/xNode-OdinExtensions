f5bb0bf617aef7ff62b9fbb32eaf324692ceb343
Kailey Joanette <kaileyjo@bigbluebubble.com>
2020-05-02 17:20:31 -0400

    Add an Odin serializable node.
    ODIN_SERIALIZER has to be defined.

---
+++ Runtime/OdinSerializer/SerializableNode.cs
@@ -0,0 +1,36 @@
+﻿
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using UnityEngine;
+
+namespace XNode.Odin
+{
+	[ShowOdinSerializedPropertiesInInspector]
+	public abstract class SerializableNode : Node, ISerializationCallbackReceiver
+	{
+		#region Odin serialized data
+		[SerializeField, HideInInspector]
+		private SerializationData serializationData;
+
+		void ISerializationCallbackReceiver.OnAfterDeserialize()
+		{
+			UnitySerializationUtility.DeserializeUnityObject( this, ref this.serializationData );
+			this.OnAfterDeserialize();
+		}
+
+		void ISerializationCallbackReceiver.OnBeforeSerialize()
+		{
+			this.OnBeforeSerialize();
+			UnitySerializationUtility.SerializeUnityObject( this, ref this.serializationData );
+		}
+		#endregion
+
+		public virtual void OnAfterDeserialize()
+		{
+		}
+
+		public virtual void OnBeforeSerialize()
+		{
+		}
+	}
+}
