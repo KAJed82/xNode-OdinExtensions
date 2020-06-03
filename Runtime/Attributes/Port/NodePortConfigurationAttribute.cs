fab8ed96570bb7e7e2edd1cd5f79d382046ce1ba
Kailey Joanette <kaileyjo@bigbluebubble.com>
2020-06-03 11:28:59 -0400

    Added a way to make a port be nothing but a port so it can live on the same line as whatever comes next.

---
+++ Runtime/Attributes/Port/NodePortConfigurationAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace XNode.Odin
+{
+	public class NodePortConfigurationAttribute : NodePortAttribute
+	{
+		/// <summary>
+		/// Useful for ports that only want to be connections and not hold data in any way.
+		/// </summary>
+		public bool HideContents { get; protected set; }
+
+		public NodePortConfigurationAttribute( bool hideContents )
+		{
+			HideContents = hideContents;
+		}
+	}
+}
