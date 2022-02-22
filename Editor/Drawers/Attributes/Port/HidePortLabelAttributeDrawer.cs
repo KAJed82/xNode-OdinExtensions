730784763675677dae5d3e915fc1e63cf4d644ef
Kailey Joanette <kailey.joanette@gmail.com>
2022-02-22 07:55:43 -0500

    Added HidePortLabelAttribute

---
+++ Editor/Drawers/Attributes/Port/HidePortLabelAttributeDrawer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using XNode.Odin;
+
+namespace XNodeEditor.Odin
+{
+	[NodePortDrawerPriority( 2 )]
+	public class HidePortLabelAttributeDrawer : NodePortAttributeDrawer<HidePortLabelAttribute>
+	{
+		protected override void DrawPort( GUIContent label )
+		{
+			this.CallNextDrawer( null );
+		}
+	}
+}
